99516e619add1a49960b7ff0390f7e24f198e88e
kg76621-prog <kg76621@gmail.com>
2025-09-18 15:11:23 +0900

    Add files via upload

---
+++ chap 141.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture('tracking2.avi')
+while(cap.isOpened()):
+    
+    rat, frame = cap.read()
+    
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lower_blue = np.array([10,128,128])
+    upper_blue = np.array([60,255,255])
+
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
+
+#green = np.uint8([[[255,0,0 ]]])
+#hsv_green = cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
+#print (hsv_green)
